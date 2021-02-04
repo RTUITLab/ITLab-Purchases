08f7625992fa0359d86e74b49792c37602d09fc9
Nik <allyru@yandex.ru>
2021-02-04 12:13:36 +0300

    add archive

--- src/main/java/com/service/Service/Update.java
+++ src/main/java/com/service/Service/Update.java
@@ -4,6 +4,10 @@ import com.service.Items.Data;
 import org.springframework.http.HttpStatus;
 
 import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Update {
 
@@ -15,9 +19,44 @@ public class Update {
     {
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
 
+            Data dt;
+            Date dateTimeInDB = new Date();
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd hh-mm-ss");
+
+            String stringTimeInDB = df.format(dateTimeInDB);
+
             Statement stmtObj = connection.createStatement();
+
+            ResultSet resObj = stmtObj.executeQuery("SELECT  expenses.id, costname.name, expenses.price, expenses.datePurchase, expenses.amount, " +
+                    "expenses.dateTimeCreate, expenses.dateTimeInDB, expenses.userName " +
+                    "FROM lab_expenses.expenses " +
+                    "inner join lab_expenses.costname on costname.id = expenses.idCname " +
+                    "where expenses.id = \""+data.getId()+"\"");
+
+                resObj.next();
+
+                dt = new Data(
+                        resObj.getInt("id"),
+                        resObj.getString("name"),
+                        resObj.getFloat("price"),
+                        resObj.getString("datePurchase"),
+                        resObj.getInt("amount"),
+                        resObj.getString("dateTimeCreate"),
+                        resObj.getString("dateTimeInDB"),
+                        resObj.getString("userName"));
+
+
+            stmtObj.execute("insert into lab_expenses.archive (idExpenses, name, price, datePurchase, amount, " +
+                    "dateTimeCreate, dateTimeInDB, userName, dateTimeInArchive) value (\""+dt.getId()+"\", \""+
+                            dt.getName()+"\", \""+dt.getPrice()+"\", \""+dt.getDatePurchase()+"\", \""+dt.getAmount()+
+                    "\", \""+dt.getDateTimeCreate()+"\", \""+dt.getDateTimeInDB()+"\", \""+dt.getUserName()+"\", \""+stringTimeInDB+"\")");
+
             stmtObj.execute("update lab_expenses.expenses set price = \""+data.getPrice()+"\", amount = \""+
                     data.getAmount()+"\", datePurchase = \""+data.getDatePurchase()+"\" where id =\""+data.getId()+"\"");
+
+
+            stmtObj.close();
+            connection.close();
 
         } catch (SQLException e) {
             System.out.println("Connection failed!");
