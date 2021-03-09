39622f4659d70def3b14a6c81bc25dd75488b5e2
Nik <allyru@yandex.ru>
2021-03-09 18:50:32 +0300

    fix

--- src/test/java/com/karate/KarateTest.java
+++ src/test/java/com/karate/KarateTest.java
@@ -4,9 +4,9 @@ import com.intuit.karate.junit5.Karate;
 
 class KarateTest {
 
-    @Karate.Test
-    Karate testCheck() {
-       return Karate.run("testingFunctional").relativeTo(getClass());
-    }
+//    @Karate.Test
+//    Karate testCheck() {
+//       return Karate.run("testingFunctional").relativeTo(getClass());
+//    }
 
 }
